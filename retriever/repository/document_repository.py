5e953bc8366b8a76f746b90f08b54227309a052b
Yoonseo Han <yooncer00@gmail.com>
2026-02-08 19:29:44 +0900

    Retrieve documents based on domain and retrieve all documents

--- retriever/repository/document_repository.py
+++ retriever/repository/document_repository.py
@@ -1,14 +1,21 @@
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker
 from typing import List
 
 from retriever.models import Document
 
 
 class DocumentRepository:
-    def __init__(self, db: Session):
-        self.db = db
+    def __init__(self, engine: Engine):
+        self._session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     def get_documents(self, domain: str) -> List[Document]:
-        stmt = select(Document).where(Document.domain == domain)
-        return list(self.db.execute(stmt).scalars().all())
+        with self._session_factory() as session:
+            stmt = select(Document).where(Document.domain == domain)
+            return list(session.execute(stmt).scalars().all())
+
+    def get_all_documents(self) -> List[Document]:
+        with self._session_factory() as session:
+            stmt = select(Document)
+            return list(session.execute(stmt).scalars().all())
