8e14328a1f85517e618cade0589aa1ce1804907f
Yoonseo Han <yooncer00@gmail.com>
2026-02-08 15:53:46 +0900

    Test commit

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -24,7 +24,7 @@ class SimpleRetriever:
 
     def _embed_documents(self):
         self.embeddings = self.model.encode(self.documents, convert_to_numpy=True, show_progress_bar=True, normalize_embeddings=True)
-        dimension = self.embeddings.shape[1]
+        dimension = self.embeddings.shape[1
         self.index = faiss.IndexFlatIP(dimension)
         self.index.add(self.embeddings.astype('float32'))
         print(f"Indexed {len(self.documents)} documents.")
